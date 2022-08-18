ec93f328e81ef389ffa774bba2209e1c7593c3d2
Vi Phan <vipt@younetgroup.com>
2022-08-18 12:15:23 +0700

    test1

--- src/components/FullWidthImage.js
+++ src/components/FullWidthImage.js
@@ -70,7 +70,7 @@ export default function FullWidthImage(props) {
                 style={{
                   boxShadow:
                     "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
-                  backgroundColor: "rgb(255, 68, 0)",
+                  backgroundColor: "lightblue",
                   color: "white",
                   lineHeight: "1",
                   padding: "0.25em",
