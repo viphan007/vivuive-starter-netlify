5a0cc1ba98621f64c89bb18db712e99a751dacea
viphan007 <vipt@younetgroup.com>
2023-11-24 17:18:16 +0700

    Update social share image

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -152,8 +152,8 @@ export function Head() {
   return (
     <>
       <title>Test home page</title>
-      <meta name="twitter:image" content="https://images.ctfassets.net/gjyjx7gst9lo/6sfeuOQgcPogLE4FKUfztr/472352cdb1c95aec4472e4a9fdcd0a0f/pic_2.jpg?w=1200&h=630" />
-      <meta property="og:image" content="https://images.ctfassets.net/gjyjx7gst9lo/6sfeuOQgcPogLE4FKUfztr/472352cdb1c95aec4472e4a9fdcd0a0f/pic_2.jpg?w=1200&h=630" />
+      <meta name="twitter:image" content="https://images.ctfassets.net/gjyjx7gst9lo/6sfeuOQgcPogLE4FKUfztr/472352cdb1c95aec4472e4a9fdcd0a0f/pic_2.jpg" />
+      <meta property="og:image" content="https://images.ctfassets.net/gjyjx7gst9lo/6sfeuOQgcPogLE4FKUfztr/472352cdb1c95aec4472e4a9fdcd0a0f/pic_2.jpg" />
       <meta name="description" content="description" />
       <meta property="og:title" content="og:title og:title og:title" />
       <meta property="og:description" content="og:description og:description og:description og:description" />
